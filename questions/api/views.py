2e345ccd8f96e5f0c751682482b72b9654019f18
DevSpace88 <sgorochow@hotmail.de>
2023-06-01 16:20:51 +0200

    added further answer- and like functionality/endpoints

--- questions/api/views.py
+++ questions/api/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, status, viewsets
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from questions.api.permissions import isAuthorOrReadOnly
 from questions.api.serializers import AnswerSerializer, QuestionSerializer
@@ -35,3 +37,41 @@ class AnswerCreateAPIView(generics.CreateAPIView):
 
         serializer.save(author=request_user, question=question)
         
+class AnswerRUDAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated, isAuthorOrReadOnly]
+    
+    lookup_field = "uuid"
+    
+class AnswerListAPIView(generics.ListAPIView):
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated, isAuthorOrReadOnly]
+    
+    def get_queryset(self):
+        kwarg_slug = self.kwargs.get("slug")
+        return Answer.objects.filter(question__slug=kwarg_slug).order_by("-created_at")
+    
+class AnswerLikeAPIView(APIView):
+    serializer_class = AnswerSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request, uuid):
+        answer = get_object_or_404(Answer, uuid=uuid)
+        answer.voters.add(request.user)
+        answer.save()
+        
+        serializer_context = {"request": request}
+        serializer = self.serializer_class(answer, context=serializer_context)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, uuid):
+        answer = get_object_or_404(Answer, uuid=uuid)
+        answer.voters.remove(request.user)
+        answer.save()
+        
+        serializer_context = {"request": request}
+        serializer = self.serializer_class(answer, context=serializer_context)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
