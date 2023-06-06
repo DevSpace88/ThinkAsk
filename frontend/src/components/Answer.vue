52fe1bc58879684e00a047b81a1967621e434790
DevSpace88 <sgorochow@hotmail.de>
2023-06-06 19:27:11 +0200

    added Answer-Functionality/Form

---
+++ frontend/src/components/Answer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p class="text-muted">
+            <strong>{{ answer.author }} &#8901; {{ answer.created_at }}</strong>
+        </p>
+        <p class="white-space: pre-wrap;">{{ answer.body }}</p>
+        <hr>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "AnswerComponent",
+    props: {
+        answer: {
+            type: Object, 
+            required: true,
+        }
+    }
+};
+</script>
+
+
+<style>
+</style>
--- frontend/src/views/QuestionView.vue
+++ frontend/src/views/QuestionView.vue
@@ -4,15 +4,46 @@
             <h1>{{ question.content }}</h1>
             <p class="mb-0">Posted by: <span class="author-name">{{ question.author }}</span></p>
             <p>{{ question.created_at }}</p>
+
+            <div v-if="userHasAnswered">
+                <p class="answer-added">Yo've written an answer!</p>
+            </div>
+            <div v-else-if="showForm">
+                <form @submit.prevent="onSubmit">
+                    <p>Answer the Question</p>
+                    <textarea v-model="newAnswerBody" class="form-control" placeholder="Share your Knowledge"
+                        rows="10"></textarea>
+                    <button type="submit" class="btn btn-outline-primary my-3">Submit your Answer</button>
+                </form>
+                <p v-if="error" class="error mt-2">{{ error }}</p>
+            </div>
+            <div v-else>
+                <button class="btn btn-outline-primary" @click="showForm = true">
+                    Answer the Question!
+                </button>
+            </div>
+
+            <hr>
         </div>
         <div v-else>
             <h1 class="error text-center">404 - Question Not Found</h1>
         </div>
+
+        <div v-if="question">
+            <AnswerComponent v-for="answer in answers" :key="answer.uuid" :answer="answer" />
+        </div>
+
+        <div class="my-4">
+            <p v-show="loadingAnswers">...loading...</p>
+            <button v-show="next" @click="getQuestionAnswers" class="btn btn-sm btn-outline-primary">Load More</button>
+        </div>
+
     </div>
 </template>
 
 <script>
 import { axios } from "@/common/api.service.js";
+import AnswerComponent from "@/components/Answer.vue";
 export default {
     name: "QuestionView",
     props: {
@@ -21,13 +52,23 @@ export default {
             required: true,
         },
     },
+    components: {
+        AnswerComponent
+    },
     data() {
         return {
-            question: null
+            question: null,
+            answers: [],
+            next: null,
+            loadingAnswers: false,
+            userHasAnswered: false,
+            showForm: false,
+            newAnswerBody: null,
+            error: null
         }
     },
     methods: {
-        setPageTitle(title){
+        setPageTitle(title) {
             document.title = title;
         },
         async getQuestionData() {
@@ -35,16 +76,61 @@ export default {
             try {
                 const response = await axios.get(endpoint);
                 this.question = response.data;
+                this.userHasAnswered = response.data.user_has_answered;
                 this.setPageTitle(response.data.content);
             } catch (error) {
                 console.log(error.response);
                 const title = "404 - not Found!"
                 this.setPageTitle(title);
             }
+        },
+        async getQuestionAnswers() {
+            let endpoint = `/api/v1/questions/${this.slug}/answers/`;
+            if (this.next) {
+                endpoint = this.next;
+            }
+            this.loadingAnswers = true;
+            try {
+                const response = await axios.get(endpoint);
+                this.answers.push(...response.data.results);
+                console.log(this.answers);
+                this.loadingAnswers = false;
+                if (response.data.next) {
+                    this.next = response.data.next;
+                } else {
+                    this.next = null;
+                }
+            } catch (error) {
+                console.log(error.response);
+                alert(error.response.statusText);
+            }
+        },
+        async onSubmit() {
+            if (!this.newAnswerBody) {
+                this.error = "You can't send an empty answer!"
+                return
+            }
+            const endpoint = `/api/v1/questions/${this.slug}/answer/`;
+            try {
+                const response = await axios.post(endpoint, { 
+                    body: this.newAnswerBody 
+                });
+                this.answers.unshift(response.data)
+                this.newAnswerBody = null;
+                this.showForm = false;
+                this.userHasAnswered = true;
+                if (this.error) {
+                    this.error = null;
+                }
+            } catch (error) {
+                console.log(error.response);
+                alert(error.response.statusText);
+            }
         }
     },
     created() {
         this.getQuestionData();
+        this.getQuestionAnswers();
     }
 };
 </script>
@@ -57,5 +143,10 @@ export default {
 
 .error {
     color: #dc3545;
+}
+
+.answer-added {
+    font-weight: bold;
+    color: green;
 }
 </style>
